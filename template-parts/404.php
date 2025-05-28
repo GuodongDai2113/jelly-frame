0be5671b5f7af3a1c03a2bed1578581333cb2ab3
JellyDai <daiguo1003@gmail.com>
2025-05-28 13:44:42 +0800

    优化了布局的结构

--- template-parts/404.php
+++ template-parts/404.php
@@ -12,7 +12,7 @@
 if (! defined('ABSPATH')) exit; // 禁止直接访问
 
 ?>
-<main id="content" role="main" class="error-page">
+<main id="main" role="main" class="error-page">
     <h1 class="error-title"><?php echo esc_html__('404', 'jelly-frame') ?></h1>
     <h2 class="error-description"><?php echo esc_html__('Page Not Find', 'jelly-frame') ?></h2>
     <p class="error-description"><?php echo esc_html__('Sorry, the page you are looking for does not exist.', 'jelly-frame') ?></p>
